597af1293c6f898046eef36ac00439fa600a9ffe
TuanAnh_VN <lt-anh@b-create.co.jp>
2025-08-20 10:10:59 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,14 @@ const greatVibes = Great_Vibes({
 export const metadata: Metadata = {
   title: "Lê Tuấn Anh & Phạm Thì Kiều - Lễ Cưới 01/09",
   description: "Hãy cùng chúc phúc cho hành trình yêu thương của Lê Tuấn Anh và Phạm Thì Kiều trong ngày cưới đặc biệt - 01/09/2024.",
+  icons: {
+    icon: '/favicon.png',
+  },
   openGraph: {
     title: "Lê Tuấn Anh & Phạm Thì Kiều - Lễ Cưới 01/09/2024",
     description: "Hãy cùng chúc phúc cho hành trình yêu thương của Lê Tuấn Anh và Phạm Thì Kiều trong ngày cưới đặc biệt - 01/09/2024.",
     url: "https://anh-kieu-wedding.vercel.app/",
     siteName: "Thiệp cưới Lê Tuấn Anh & Phạm Thì Kiều",
-    icons: {
-      icon: '/favicon.png',
-    },
     images: [
       {
         url: "https://anh-kieu-wedding.vercel.app/og-image.jpg",
